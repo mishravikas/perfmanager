3ebbcbddfa2a4a8de37d832993c42c4c2d57581e
Vikas Mishra <vikasmishra95@gmail.com>
2015-03-07 19:04:40 +0530

    Added merged alerts

--- perfmanager/urls.py
+++ perfmanager/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'perfmanager.views.index'),
     url(r'^alertsbyrev/$', 'perfmanager.views.alertsbyrev'),
+    url(r'^mergedalerts/$', 'perfmanager.views.mergedalerts'),
     url(r'^admin/', include(admin.site.urls)),
     (r'^accounts/', include('accounts.urls')),
     (r'', include('django_browserid.urls')),
